2fa577a8743ecc52f423a7074ed12e8a158c5be2
Mohammad abir Abbas <mabbas@c1r2s6.42wolfsburg.de>
2022-10-28 20:11:41 +0200

    First Edit -- maybe give the directory common

---
+++ terminal_.fun.sh
@@ -0,0 +1,52 @@
+
+!/bin/sh
+clear
+
+say -v whisper Welcome !!
+
+printf "\t\t\t\t You know who is cooking something!_!" | /goinfre/mabbas/nms/bin/nms -af green
+printf "\n"
+
+sleep 0.3
+say -v whisper Wanna see.....
+clear
+
+printf "\t\t\t\t\t For the 42 People  !_!" | /goinfre/mabbas/nms/bin/nms -af magenta
+printf "\n"
+
+say -v whisper Whats happening 
+sleep 0.3
+
+printf "                                  
+                               :JGBBBBBBGJ:   7BBBBBBB5?GBBBBBBBB~                                  
+                             :JB&####&#Y^     ?&##&&P! ^########&!                                  
+                           ^Y#&####&BJ:       ?&&#5~   ^#########!                                  
+                         ~5#&####&B?:         ?#5~     ^########&!                                  
+                       !P#&####&G?.           ^^      .J########P^                                  
+                    .!P#######G7.                   :JB#######P!                                    
+                  .7G#######P!.                   ^JB&####&#5~                                      
+                .?G&######5~                    ^Y#&####&#Y^                                        
+              .JB&######BJ~~~~~~~~~~~~~~~~^   :5#&######Y^      ..                                  
+              :##########################&G   !&#######B.     .?B!                                  
+              :###########################P   !########B:   :JB&&!                                  
+              :#&&&&&&&&&&&&&&&&&#########P   !########B. ^YB&###!                                  
+              .!!!!!!!!!!!!!!!!!!P########P   !########B~Y#&#####!                                  
+                                 Y&#######P   .^^^^^^^^^^^^^^^^^^.                                  
+                                 5&#######P                                                         
+                                 5&&&&&&&&G                                                         
+                                 75YYYYYY5J" | /goinfre/mabbas/nms/bin/nms -af black
+
+printf "\n"
+
+say -v whisper [[volm +0.3]] Happy Halloween;
+sleep 0.3
+
+printf " 
+		42WoB	42WOB	42WOB	42WOB	242W4O	42WOB	42WOB	42WOB	42WOB
+			█████████████████████████████████████████████████████████▀█
+			█▄─█▀▀▀█─▄█─▄▄─█▄─▄███▄─▄▄─█─▄▄▄▄█▄─▄─▀█▄─██─▄█▄─▄▄▀█─▄▄▄▄█
+			██─█─█─█─██─██─██─██▀██─▄███▄▄▄▄─██─▄─▀██─██─███─▄─▄█─██▄─█
+			▀▀▄▄▄▀▄▄▄▀▀▄▄▄▄▀▄▄▄▄▄▀▄▄▄▀▀▀▄▄▄▄▄▀▄▄▄▄▀▀▀▄▄▄▄▀▀▄▄▀▄▄▀▄▄▄▄▄▀
+		42WOB	42WOB	42WOB	42WOB	42W4O	42WOB	42WOB	42WOB	42WOB
+				!_! LETS  CODE !_!" | /goinfre/mabbas/nms/bin/nms -af black;
+say -v Whisper Ha ha hah he hehe  [[volm +1.11]]
